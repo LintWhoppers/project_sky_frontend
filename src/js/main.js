18ab5d666c6e5d823efefcea1e8731077227f64e
Apavayan Sinha <info@apavayan.com>
2022-10-21 09:42:20 +0530

    Update Darkmode/light mode logic

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,9 @@
+// TODO: For Debug, Will remove in Production
+localStorage.clear();
+
 if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
     document.getElementById("thememode").checked = true
-    document.documentElement.setAttribute('data-theme', 'dark')
+    document.documentElement.setAttribute('data-theme', 'halloween')
 } else {
     document.documentElement.setAttribute('data-theme', 'cupcake')
 }
@@ -9,7 +12,7 @@ function toggleTheme() {
     console.log("on Click")
     if (document.getElementById("thememode").checked) {
         localStorage.setItem('theme', 'dark')
-        document.documentElement.setAttribute('data-theme', 'dark')
+        document.documentElement.setAttribute('data-theme', 'halloween')
     } else {
         localStorage.setItem('theme', 'light')
         document.documentElement.setAttribute('data-theme', 'cupcake')
