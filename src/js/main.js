4dcdc07b43ad0ede7081e768d3c7f088a9f77650
Apavayan Sinha <info@apavayan.com>
2022-10-29 13:52:16 +0530

    Tweaked Go To Top method

--- src/js/main.js
+++ src/js/main.js
@@ -46,3 +46,21 @@ counters.forEach(counter => {
 
     updateCount();
 });
+
+
+let mybutton = document.getElementById("backToTop");
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+        mybutton.style.display = "grid";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
