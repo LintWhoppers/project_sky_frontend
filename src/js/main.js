4230492fe543c62af24e7b63c1125702ca9ed370
Apavayan Sinha <info@apavayan.com>
2022-10-29 14:34:59 +0530

    Js: Using Custom Speed for each stats

--- src/js/main.js
+++ src/js/main.js
@@ -20,18 +20,15 @@ function toggleTheme() {
 }
 
 const counters = document.querySelectorAll('.counter');
-const speed = 2000; // The lower the slower
 
 counters.forEach(counter => {
     const updateCount = () => {
         const target = +counter.getAttribute('data-target');
+        const speed = +counter.getAttribute('data-speed');
         const count = +counter.innerText;
 
         // Lower inc to slow and higher to slow
         const inc = target / speed;
-
-        // console.log(inc);
-        // console.log(count);
 
         // Check if target is reached
         if (count < target) {
